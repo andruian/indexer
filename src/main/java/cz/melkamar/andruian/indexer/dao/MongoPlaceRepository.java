be48cf064da00c5c04ed68f212480c526ef23894
Martin Melka <melkamar@fit.cvut.cz>
2018-03-02 17:56:07 +0100

    Add Place-storing methods

--- src/main/java/cz/melkamar/andruian/indexer/dao/MongoPlaceRepository.java
+++ src/main/java/cz/melkamar/andruian/indexer/dao/MongoPlaceRepository.java
@@ -3,8 +3,10 @@ package cz.melkamar.andruian.indexer.dao;
 import cz.melkamar.andruian.indexer.model.place.Place;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MongoPlaceRepository extends MongoRepository<Place, String> {
     Optional<Place> findByUri(String uri);
+    List<Place> findAllByClassUri(String classUri);
 }
--- src/main/java/cz/melkamar/andruian/indexer/dao/PlaceDAO.java
+++ src/main/java/cz/melkamar/andruian/indexer/dao/PlaceDAO.java
@@ -2,12 +2,46 @@ package cz.melkamar.andruian.indexer.dao;
 
 import cz.melkamar.andruian.indexer.exception.NotImplementedException;
 import cz.melkamar.andruian.indexer.model.place.Place;
+import cz.melkamar.andruian.indexer.model.place.SolrPlace;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Class orchestrating storing and retrieving {@link Place} objects.
+ * <p>
+ * They are stored in two separate databases:
+ * - Solr for fast spatial queries (only location and uri)
+ * - MongoDB for the whole objects, without indexing.
+ * <p>
+ * Methods of this class will ensure that both databases are used in unison.
+ */
+
+@Component
 public class PlaceDAO {
+    private final MongoPlaceRepository mongoPlaceRepository;
+    private final SolrPlaceRepository solrPlaceRepository;
+
+    @Autowired
+    public PlaceDAO(MongoPlaceRepository mongoPlaceRepository,
+                    SolrPlaceRepository solrPlaceRepository) {
+        this.mongoPlaceRepository = mongoPlaceRepository;
+        this.solrPlaceRepository = solrPlaceRepository;
+    }
+
     public void savePlace(Place place) {
-        throw new NotImplementedException();
+        solrPlaceRepository.save(new SolrPlace(place));
+        mongoPlaceRepository.save(place);
+    }
+
+    public void savePlaces(List<Place> places) {
+        solrPlaceRepository.saveAll(places.stream().map(SolrPlace::new).collect(Collectors.toList()));
+        mongoPlaceRepository.saveAll(places);
     }
 
     public void deletePlace(Place place) {
@@ -19,14 +53,22 @@ public class PlaceDAO {
     }
 
     public List<Place> getPlacesOfClass(String classUri) {
-        throw new NotImplementedException();
+        return mongoPlaceRepository.findAllByClassUri(classUri);
     }
 
     public List<Place> getPlacesAroundPoint(double latCoord, double longCoord, double radius) {
         throw new NotImplementedException();
     }
 
-    public List<Place> getAllPlaces(String classUri, double latCoord, double longCoord, double radius) {
-        throw new NotImplementedException();
+    public List<Place> getPlacesAroundPointOfClass(String classUri, double latCoord, double longCoord, double radius) {
+        List<SolrPlace> solrPlaces = solrPlaceRepository.findByTypeAndLocationWithin(classUri,
+                                                                                     new Point(latCoord, longCoord),
+                                                                                     new Distance(radius));
+        List<Place> result = new ArrayList<>(solrPlaces.size());
+        for (SolrPlace solrPlace: solrPlaces){
+            mongoPlaceRepository.findByUri(solrPlace.getUri()).ifPresent(result::add);
+        }
+        
+        return result;
     }
 }
