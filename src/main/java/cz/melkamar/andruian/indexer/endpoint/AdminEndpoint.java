e47d61c5be7c4528f349108da895a2bba08828b8
Martin Melka <melkamar@fit.cvut.cz>
2018-03-02 12:51:15 +0100

    Add logging

--- src/main/java/cz/melkamar/andruian/indexer/endpoint/AdminEndpoint.java
+++ src/main/java/cz/melkamar/andruian/indexer/endpoint/AdminEndpoint.java
@@ -3,6 +3,8 @@ package cz.melkamar.andruian.indexer.endpoint;
 import cz.melkamar.andruian.indexer.dao.DataDefDAO;
 import cz.melkamar.andruian.indexer.model.datadef.DataDef;
 import cz.melkamar.andruian.indexer.service.IndexService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/admin")
 public class AdminEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdminEndpoint.class);
+    
     private final DataDefDAO dataDefDAO;
     private final IndexService indexService;
 
@@ -23,6 +27,8 @@ public class AdminEndpoint {
 
     @RequestMapping("reindex")
     public String reindex(@RequestParam(value = "dataDefUri", required = false, defaultValue = "") String dataDefUri) {
+        LOGGER.info("Reindex uri: '{}'", dataDefUri);
+        
         if (dataDefUri != null && dataDefUri.length() > 0) {
             DataDef dataDef = dataDefDAO.getDataDefFromUri(dataDefUri);
             indexService.indexDataDef(dataDef);
