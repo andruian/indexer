6f4b9f535fe7c70a6ce1cea229a48795d9dcc511
Martin Melka <melkamar@fit.cvut.cz>
2018-03-17 23:02:29 +0100

    Remove some unused jena stuff

--- src/main/java/cz/melkamar/andruian/indexer/Util.java
+++ src/main/java/cz/melkamar/andruian/indexer/Util.java
@@ -2,26 +2,14 @@ package cz.melkamar.andruian.indexer;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Util {
     public static String readResourceFileToString(String resourcePath) throws IOException {
         ClassLoader classLoader = Util.class.getClassLoader();
         return IOUtils.toString(classLoader.getResourceAsStream(resourcePath), "utf-8");
-    }
-
-    public static Model readModelFromResource(String resourcePath, Class clazz) throws FileNotFoundException {
-        ClassLoader classLoader = clazz.getClassLoader();
-        File file = new File(classLoader.getResource(resourcePath).getFile());
-        Model model = ModelFactory.createDefaultModel();
-        model.read(new FileInputStream(file), null, "TURTLE");
-        return model;
     }
 
     public static String readStringFromResource(String resourcePath, Class clazz) throws IOException {
