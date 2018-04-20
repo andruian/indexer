4ddadfc6b796ccfad4c467724254d2847e6e02f9
Melka Martin <melka@avast.com>
2018-04-20 14:15:34 +0200

    Add javadoc for clustering functionality.

--- src/main/java/cz/melkamar/andruian/indexer/dao/ClusterQueryBuilder.java
+++ src/main/java/cz/melkamar/andruian/indexer/dao/ClusterQueryBuilder.java
@@ -3,6 +3,12 @@ package cz.melkamar.andruian.indexer.dao;
 import cz.melkamar.andruian.indexer.Util;
 import org.springframework.web.util.UriComponentsBuilder;
 
+/**
+ * A builder for Solr heatmap-faceted query.
+ *
+ * The query's parameters are identical to a regular query, but contain a few extra parameters. See the {@link #build()}
+ * method for more information.
+ */
 public class ClusterQueryBuilder {
     private final String solrUrl;
     private final String collection;
@@ -13,8 +19,8 @@ public class ClusterQueryBuilder {
     private double distErrPct = 0.50;
 
     /**
-     * @param solrUrl
-     * @param collection
+     * @param solrUrl    The URL of the solr instance, e.g. https://localhost:8983/solr.
+     * @param collection The name of the collection containing indexer data, e.g. andruian.
      */
     public ClusterQueryBuilder(String solrUrl, String collection) {
         this.solrUrl = solrUrl;
@@ -22,8 +28,9 @@ public class ClusterQueryBuilder {
     }
 
     /**
-     * @param solrUrl
-     * @param collection
+     * @param solrUrl    The URL of the solr instance, e.g. https://localhost:8983/solr.
+     * @param collection The name of the collection containing indexer data, e.g. andruian.
+     * @param distErrPct The facet.heatmap.distErrPct value of the query, see <a href="https://lucene.apache.org/solr/guide/6_6/spatial-search.html#SpatialSearch-HeatmapFaceting">Solr heatmap faceting</a>.
      */
     public ClusterQueryBuilder(String solrUrl, String collection, double distErrPct) {
         this.solrUrl = solrUrl;
@@ -31,11 +38,24 @@ public class ClusterQueryBuilder {
         this.distErrPct = distErrPct;
     }
 
+    /**
+     * Set the type (RDF class) of objects to search for.
+     *
+     * @param type The IRI of the class to search for.
+     */
     public ClusterQueryBuilder setType(String type) {
         this.type = type;
         return this;
     }
 
+    /**
+     * Set the location around which to search for objects.
+     *
+     * @param lat    The latitude of the area around which to search.
+     * @param lng    The longitude of the area around which to search.
+     * @param radius The radius of the area in which to search, in kilometers.
+     * @return
+     */
     public ClusterQueryBuilder setLocation(double lat, double lng, double radius) {
         this.lat = lat;
         this.lng = lng;
@@ -44,6 +64,11 @@ public class ClusterQueryBuilder {
     }
 
     /**
+     * Build a Solr heatmap query. See <a href="https://lucene.apache.org/solr/guide/6_6/spatial-search.html#SpatialSearch-HeatmapFaceting">Solr heatmap faceting</a>
+     * for more details.
+     *
+     * The query may look like the following:
+     * <pre>
      * http://localhost:8983/solr/andruian/select?
      * d=2000 &
      * facet.heatmap.geom=[%2214.411%2050.0%22%20TO%20%2214.412%2050.01%22] &
@@ -53,6 +78,12 @@ public class ClusterQueryBuilder {
      * fq={!bbox%20sfield=location} &
      * pt=50.052828,14.439898 &
      * q=type:%22http://example.org/SourceObjectA%22
+     * </pre>
+     *
+     * <b>NOTE:</b> The query is already URL-encoded. When using in a HTTP client, make sure it does not re-encode
+     * it, because that would not work.
+     *
+     * @return An encoded URL - the heatmap GET query.
      */
     public String build() {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(solrUrl);
@@ -71,9 +102,9 @@ public class ClusterQueryBuilder {
 
             Util.Rect boundRect = calculateBoundingRect(lat, lng, radius);
             builder.queryParam("facet.heatmap.geom",
-                               "[\"" + boundRect.minX + " " + boundRect.minY + "\" " +
-                                       "TO " +
-                                       "\"" + boundRect.maxX + " " + boundRect.maxY + "\"]");
+                    "[\"" + boundRect.minX + " " + boundRect.minY + "\" " +
+                            "TO " +
+                            "\"" + boundRect.maxX + " " + boundRect.maxY + "\"]");
         }
 
         if (type != null) builder.queryParam("q", "type:\"" + type + "\"");
@@ -83,12 +114,14 @@ public class ClusterQueryBuilder {
     }
 
     /**
-     * Approximate algorithm taken from here
+     * Calculate a bounding rectangle of a circle defined by the given latitude, longitude and radius.
+     *
+     * An approximate algorithm for converting kilometers into GPS "units" taken from here
      * https://stackoverflow.com/questions/1253499/simple-calculations-for-working-with-lat-lon-km-distance
      *
-     * @param lat
-     * @param lng
-     * @param radius kilometers
+     * @param lat    Latitude of the center of the circle.
+     * @param lng    Longitude of the center of the circle.
+     * @param radius The radius of the circle in kilometers.
      */
     private Util.Rect calculateBoundingRect(double lat, double lng, double radius) {
         double latDelta = radius / 110.574;
--- src/main/java/cz/melkamar/andruian/indexer/dao/ClusteredPlaceDAO.java
+++ src/main/java/cz/melkamar/andruian/indexer/dao/ClusteredPlaceDAO.java
@@ -14,6 +14,15 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A Data Access Object for retrieving clusters of places from the index database.
+ *
+ * The clustering functionality is implemented by the Solr database as heatmaps. The geospatial size
+ * of a cluster depends on the size of the area the clusters are formed from. The number of cells the
+ * given area is divided into should be roughly the same, regardless of the zoom level.
+ *
+ * If no area is provided, it defaults to encompass the whole world.
+ */
 @Component
 public class ClusteredPlaceDAO {
     private final static Logger LOGGER = LoggerFactory.getLogger(ClusteredPlaceDAO.class);
@@ -28,6 +37,11 @@ public class ClusteredPlaceDAO {
         this.configuration = configuration;
     }
 
+    /**
+     * Get all places indexed in the system as clusters.
+     *
+     * @return A list of {@link PlaceCluster} representing the matching data.
+     */
     public List<PlaceCluster> getAllPlaces() {
         LOGGER.debug("Cluster | Fetching all places");
         List<PlaceCluster> result = new ArrayList<>();
@@ -39,6 +53,12 @@ public class ClusteredPlaceDAO {
         return parseClusterQueryResponse(response);
     }
 
+    /**
+     * Get all places of the given RDF class indexed in the system as clusters.
+     *
+     * @param classUri The IRI of a RDF class to search for.
+     * @return A list of {@link PlaceCluster} representing the matching data.
+     */
     public List<PlaceCluster> getPlacesOfClass(String classUri) {
         LOGGER.debug("Cluster | Fetching places of type " + classUri);
         List<PlaceCluster> result = new ArrayList<>();
@@ -51,6 +71,14 @@ public class ClusteredPlaceDAO {
         return parseClusterQueryResponse(response);
     }
 
+    /**
+     * Get places inside an area defined by the coordinates as clusters.
+     *
+     * @param latCoord  The latitude of the center of the area to search in.
+     * @param longCoord The longitude of the center of the area to search in.
+     * @param radius    The radius from the center of the area to search in, in kilometers.
+     * @return A list of {@link PlaceCluster} representing the matching data.
+     */
     public List<PlaceCluster> getPlacesAroundPoint(double latCoord, double longCoord, double radius) {
         LOGGER.debug("Cluster | getPlacesAroundPoint {} {} {}", latCoord, longCoord, radius);
         List<PlaceCluster> result = new ArrayList<>();
@@ -63,6 +91,15 @@ public class ClusteredPlaceDAO {
         return parseClusterQueryResponse(response);
     }
 
+    /**
+     * Get places inside an area defined by the coordinates, of a particular RDF class, as clusters.
+     *
+     * @param classUri  The IRI of a RDF class to search for.
+     * @param latCoord  The latitude of the center of the area to search in.
+     * @param longCoord The longitude of the center of the area to search in.
+     * @param radius    The radius from the center of the area to search in, in kilometers.
+     * @return A list of {@link PlaceCluster} representing the matching data.
+     */
     public List<PlaceCluster> getPlacesAroundPointOfClass(String classUri,
                                                           double latCoord,
                                                           double longCoord,
@@ -79,6 +116,20 @@ public class ClusteredPlaceDAO {
         return parseClusterQueryResponse(response);
     }
 
+    /**
+     * Parse the response of a cluster/heatmap query from Solr. An example of the query format is provided
+     * in the test class {@link cz.melkamar.andruian.indexer.dao.ClusteredPlaceDAOTest}.
+     *
+     * The response does not directly return GPS locations of the clusters. Instead, it divides the rectangle
+     * that is being searched into an orthogonal grid and returns the number of columns and rows of such grid.
+     * The grid of clusters is then represented as a 2D array of numbers, each number specifying how many clusters
+     * belong to each of the grid cells. The method {@link ClusteredPlaceDAO#calculateClusterPosition(int, int, Util.Rect, int, int)}
+     * is used to calculate the GPS position of the center of each grid cell (and effectively the approximate location
+     * of the place cluster it represents).
+     *
+     * @param response The query JSON response.
+     * @return A parsed list of {@link PlaceCluster} objects.
+     */
     public List<PlaceCluster> parseClusterQueryResponse(String response) {
         List<PlaceCluster> result = new ArrayList<>();
 
@@ -95,17 +146,6 @@ public class ClusteredPlaceDAO {
         double maxY = locationJson.getDouble(13);
         JSONArray counts = locationJson.getJSONArray(15);
 
-//        for (int row = 0; row < rows; row++) {
-//            for (int col = 0; col < columns; col++) {
-//                Util.LatLng clusterPos = calculateClusterPosition(columns,
-//                                                                  rows,
-//                                                                  new Util.Rect(minX, minY, maxX, maxY),
-//                                                                  col,
-//                                                                  row);
-//                result.add(new PlaceCluster(1, clusterPos.lat, clusterPos.lng));
-//            }
-//        }
-
         for (int row = 0; row < counts.length(); row++) {
             if (counts.isNull(row)) continue;
             JSONArray rowCounts = counts.getJSONArray(row);
@@ -116,10 +156,10 @@ public class ClusteredPlaceDAO {
                 if (placeCount == 0) continue; // No place in this ROWxCOL
 
                 Util.LatLng clusterPos = calculateClusterPosition(columns,
-                                                                  rows,
-                                                                  new Util.Rect(minX, minY, maxX, maxY),
-                                                                  col,
-                                                                  row);
+                        rows,
+                        new Util.Rect(minX, minY, maxX, maxY),
+                        col,
+                        row);
                 result.add(new PlaceCluster(placeCount, clusterPos.lat, clusterPos.lng));
             }
         }
@@ -128,6 +168,22 @@ public class ClusteredPlaceDAO {
         return result;
     }
 
+    /**
+     * Calculates the position of the center of a grid cell, given the grid bounding rectangle coordinates, the
+     * number of rows and columns and the row/col index of the cell. For more explanation why, refer to the
+     * {@link #parseClusterQueryResponse(String)} method.
+     *
+     * The algorithm is straightforward - split the rectangle evenly to form a grid, find the coordinates of the edge
+     * of the cell we are looking for and add half of the cell's width/height to find its center.
+     *
+     * @param columns      The number of columns the grid contains.
+     * @param rows         The number of rows the grid contains.
+     * @param boundingRect The bounding rectangle of the whole grid. The rectangle is orthogonal and defined by the
+     *                     bottom-leftmost and top-rightmost points.
+     * @param colIdx       The column index of the cell in the grid for which to calculate the position. 0-indexed.
+     * @param rowIdx       The row index of the cell in the grid for which to calculate the position. 0-indexed.
+     * @return The latitude and longitude of the center of the given grid cell.
+     */
     public Util.LatLng calculateClusterPosition(int columns, int rows, Util.Rect boundingRect, int colIdx, int rowIdx) {
         double lat = boundingRect.maxY
                 - rowIdx * (boundingRect.maxY - boundingRect.minY) / rows
