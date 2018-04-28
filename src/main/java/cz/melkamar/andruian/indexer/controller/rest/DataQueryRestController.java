253f9772b3d18f1d39e43ba4b9c9480a10378dd1
Melka Martin <melka@avast.com>
2018-04-28 11:03:45 +0200

    Add forceCluster parameter to query API

--- src/main/java/cz/melkamar/andruian/indexer/controller/rest/DataQueryRestController.java
+++ src/main/java/cz/melkamar/andruian/indexer/controller/rest/DataQueryRestController.java
@@ -77,6 +77,7 @@ public class DataQueryRestController {
      * @param showCount    If true, only return the number of matching objects, without the actual object data.
      * @param clusterLimit The maximum number of places to return without clustering. If provided and the number of
      *                     places to be returned is higher than this number, clusters will be returned instead.
+     * @param forceCluster If true, return clustered places without even counting them first (which may be slow in some cases).
      * @return A JSON-serialized {@link QueryResponse} object describing the matching data.
      */
     @RequestMapping
@@ -86,9 +87,15 @@ public class DataQueryRestController {
             @RequestParam(value = "long", required = false) Double longitude,
             @RequestParam(value = "r", required = false) Double radius,
             @RequestParam(value = "count", required = false) boolean showCount,
-            @RequestParam(value = "clusterLimit", required = false) Integer clusterLimit
+            @RequestParam(value = "clusterLimit", required = false) Integer clusterLimit,
+            @RequestParam(value = "forceCluster", required = false) Boolean forceCluster
     ) {
         try {
+            if (forceCluster != null && forceCluster){
+                return new QueryResponse(QueryResponse.RESPONSE_CLUSTERED,
+                    queryService.clusteredQuery(type, latitude, longitude, radius));
+            }
+
             List<Place> places = queryService.query(type, latitude, longitude, radius);
             if (showCount) return new QueryResponse(QueryResponse.RESPONSE_COUNT, places.size());
 
